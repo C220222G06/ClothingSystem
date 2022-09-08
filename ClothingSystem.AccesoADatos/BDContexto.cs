27619a6bf590de32207bf4ecf268ea8248f97e45
carloscris77 <carloscolon5479garcia@outlook.es>
2022-09-08 10:12:39 -0600

    crear las clases de rol y la bdcontexto en la capa de acceso a datos

---
+++ ClothingSystem.AccesoADatos/BDContexto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ClothingSystem.EntidadesDeNegocio;
+using Microsoft.EntityFrameworkCore;
+namespace ClothingSystem.AccesoADatos
+{
+    public class BDContexto : DbContext
+    {
+        public DbSet<Rol> Rol { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-DEV01\SQLEXPRESS02;Initial Catalog=SeguridadWebdb;Integrated Security=True");
+        }
+    }
+}
