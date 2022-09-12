abfef1ea0c5f0cb5610f9919d50fdbcd884f1047
carloscris77 <carloscolon5479garcia@outlook.es>
2022-09-12 16:02:39 -0600

    crear y programar la clase de BL en la capa logica de negocios

---
+++ ClothingSystem.LogicaDeNegocio/RopaFotoBL.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//***************************
+using ClothingSystem.EntidadesDeNegocio;
+using ClothingSystem.AccesoADatos;
+namespace ClothingSystem.LogicaDeNegocio
+{
+    public class RopaFotoBL
+    {
+        #region CRUD
+        public async Task<int> CrearAsync(RopaFoto pRopaFoto)
+        {
+            return await RopaFotoDAL.CrearAsync(pRopaFoto);
+        }
+        public async Task<int> ModificarAsync(RopaFoto pRopaFoto)
+        {
+            return await RopaFotoDAL.ModificarAsync(pRopaFoto);
+        }
+        public async Task<int> EliminarAsync(RopaFoto pRopaFoto)
+        {
+            return await RopaFotoDAL.EliminarAsync(pRopaFoto);
+        }
+        public async Task<RopaFoto> ObtenerPorIdAsync(RopaFoto pRopaFoto)
+        {
+            return await RopaFotoDAL.ObtenerPorIdAsync(pRopaFoto);
+        }
+        public async Task<List<RopaFoto>> ObtenerTodosAsync()
+        {
+            return await RopaFotoDAL.ObtenerTodosAsync();
+        }
+        public async Task<List<RopaFoto>> BuscarAsync(RopaFoto pRopaFoto)
+        {
+            return await RopaFotoDAL.BuscarAsync(pRopaFoto);
+        }
+        public async Task<List<RopaFoto>> BuscarIncluirRopasAsync(RopaFoto pRopaFoto)
+        {
+            return await RopaFotoDAL.BuscarIncluirRopasAsync(pRopaFoto);
+        }
+    }
+}
+#endregion
