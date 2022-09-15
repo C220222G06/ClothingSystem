e6401292e55da0e089b9ff8fd97e3e9fdaa8f143
ADONAY1234 <moisesbonilla212@gmail.com>
2022-09-15 15:51:56 -0600

    Se creó y programó la clase RopaBL

---
+++ ClothingSystem.LogicaDeNegocio/RopaBL.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//***************************
+
+using ClothingSystem.AccesoADatos;
+using ClothingSystem.EntidadesDeNegocio;
+
+namespace ClothingSystem.LogicaDeNegocio
+{
+    public class RopaBL
+    {
+        #region CRUD
+        public async Task<int> CrearAsync(Ropa pRopa)
+        {
+            return await RopaDAL.CrearAsync(pRopa);
+        }
+        public async Task<int> ModificarAsync(Ropa pRopa)
+        {
+            return await RopaDAL.ModificarAsync(pRopa);
+        }
+        public async Task<int> EliminarAsync(Ropa pRopa)
+        {
+            return await RopaDAL.EliminarAsync(pRopa);
+        }
+        public async Task<Ropa> ObtenerPorIdAsync(Ropa pRopa)
+        {
+            return await RopaDAL.ObtenerPorIdAsync(pRopa);
+        }
+        public async Task<List<Ropa>> ObtenerTodosAsync()
+        {
+            return await RopaDAL.ObtenerTodosAsync();
+        }
+        public async Task<List<Ropa>> BuscarAsync(Ropa pRopa)
+        {
+            return await RopaDAL.BuscarAsync(pRopa);
+        }
+        #endregion
+        public async Task<List<Ropa>> BuscarIncluirMarcasAsync(Ropa pRopa)
+        {
+            return await RopaDAL.BuscarIncluirMarcasAsync(pRopa);
+        }
+    }
+}
