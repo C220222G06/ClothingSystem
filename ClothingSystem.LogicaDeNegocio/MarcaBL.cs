3a264284e588703668c661ce6715425a40480b0e
carloscris77 <carloscolon5479garcia@outlook.es>
2022-09-16 21:23:05 -0600

    programo la clase de MarcaBL en la capa de Logica de Negocio

--- ClothingSystem.LogicaDeNegocio/MarcaBL.cs
+++ ClothingSystem.LogicaDeNegocio/MarcaBL.cs
@@ -4,9 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//***************************
+using ClothingSystem.EntidadesDeNegocio;
+using ClothingSystem.AccesoADatos;
 namespace ClothingSystem.LogicaDeNegocio
 {
     public class MarcaBL
     {
+        public async Task<int> CrearAsync(Marca pMarca)
+        {
+            return await MarcaDAL.CrearAsync(pMarca);
+        }
+        public async Task<int> ModificarAsync(Marca pMarca)
+        {
+            return await MarcaDAL.ModificarAsync(pMarca);
+        }
+        public async Task<int> EliminarAsync(Marca pMarca)
+        {
+            return await MarcaDAL.EliminarAsync(pMarca);
+        }
+        public async Task<Marca> ObtenerPorIdAsync(Marca pMarca)
+        {
+            return await MarcaDAL.ObtenerPorIdAsync(pMarca);
+        }
+        public async Task<List<Marca>> ObtenerTodosAsync()
+        {
+            return await MarcaDAL.ObtenerTodosAsync();
+        }
+        public async Task<List<Marca>> BuscarAsync(Marca pMarca)
+        {
+            return await MarcaDAL.BuscarAsync(pMarca);
+        }
     }
 }
